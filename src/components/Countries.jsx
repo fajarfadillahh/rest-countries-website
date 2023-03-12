e8ea461de9ee4f40dc78c37f2840ff87ae951b60
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-12 13:43:35 +0700

    fixed: Countries & CountryFilter components

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -25,23 +25,20 @@ const Countries = () => {
 
     getAllCountries();
   }, []);
-
   return (
     <section className="section pt-32">
       <div className="container grid gap-8">
         <div className="grid gap-12 lg:grid-cols-2">
-          <CountrySearch setCountries={setCountries} />
+          <CountrySearch />
 
           <CountryFilter />
         </div>
 
-        {countries && (
-          <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {countries.slice(0, 48).map((country) => {
-              return <CountryCard key={country.alpha2Code} country={country} />;
-            })}
-          </div>
-        )}
+        <div className="grid gap-12 justify-self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {countries.slice(0, 48).map((country) => (
+            <CountryCard key={country.alpha2Code} country={country} />
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/components/CountryFilter.jsx
+++ src/components/CountryFilter.jsx
@@ -3,22 +3,22 @@ import { Menu, Transition } from "@headlessui/react";
 import { RiArrowDownSLine } from "react-icons/ri";
 
 // region list
-const regionList = [
+const continents = [
   { name: "All" },
   { name: "Africa" },
-  { name: "America" },
+  { name: "Americas" },
   { name: "Asia" },
   { name: "Europe" },
   { name: "Oceania" },
 ];
 
 const CountryFilter = () => {
-  const [regions, setRegions] = useState("All");
+  const [selectedContinent, setSelectedContinent] = useState("All");
 
   return (
     <Menu as="div" className="relative z-10 w-max lg:justify-self-end">
       <Menu.Button className="inline-flex h-[56px] w-[210px] items-center justify-between rounded-md bg-white px-6 font-semibold text-gray-900 shadow-sm transition-all duration-300 dark:bg-gray-700 dark:text-white">
-        {regions === "All" ? "Filter by region" : regions}
+        {selectedContinent === "All" ? "Filter by region" : selectedContinent}
         <RiArrowDownSLine size="1.3rem" />
       </Menu.Button>
 
@@ -35,15 +35,15 @@ const CountryFilter = () => {
           as="ul"
           className="absolute mt-2 flex w-full flex-col gap-1 rounded-md bg-white py-4 px-6 shadow-sm dark:bg-gray-700"
         >
-          {regionList.map((region, index) => {
+          {continents.map((continent, index) => {
             return (
               <Menu.Item
                 as="li"
                 key={index}
-                onClick={() => setRegions(region.name)}
+                onClick={() => setSelectedContinent(continent.name)}
                 className="cursor-pointer font-semibold text-gray-900 hover:text-gray-600 dark:text-white dark:hover:text-gray-500"
               >
-                {region.name}
+                {continent.name}
               </Menu.Item>
             );
           })}
