944a27b15332e707221b3b736f132027dd2b5592
fajarfadillahh <fajaragustian979@gmail.com>
2023-03-13 23:16:50 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 
+// import components
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/country/:countryCode" element={<Details />} />
-      </Routes>
+      <Header />
+
+      <main className="mx-auto min-h-screen max-w-[1440px] overflow-hidden bg-gray-500 transition-all duration-300 dark:bg-gray-800">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/country/:countryCode" element={<Details />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 // import components
-import Header from "../components/Header";
 import Countries from "../components/Countries";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <main className="mx-auto min-h-screen max-w-[1440px] overflow-hidden bg-gray-500 transition-all duration-300 dark:bg-gray-800">
-        <Countries />
-      </main>
+      <Countries />
     </>
   );
 };
